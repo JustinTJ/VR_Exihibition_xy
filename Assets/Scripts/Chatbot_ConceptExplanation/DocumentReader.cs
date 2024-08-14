91f2c0f6c0741856e052fc95edbfe7f6babcb11f
JustinTJ <953351559@qq.com>
2024-08-15 03:36:59 +0800

    新增Chatbot + RAG 概念讲解系统

---
+++ Assets/Scripts/Chatbot_ConceptExplanation/DocumentReader.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class DocumentReader01 : MonoBehaviour
+{
+    public string filePath = "E:/A_Unity works/VR_exihibition_xy/Assets/Document/DesignConcept.txt"; // 使用相对路径
+
+    private string documentContent;
+
+    void Start()
+    {
+        ReadDocument();
+    }
+
+    void ReadDocument()
+    {
+        Debug.Log("Checking file at: " + filePath);
+
+        if (File.Exists(filePath))
+        {
+            documentContent = File.ReadAllText(filePath);
+            Debug.Log("Document Content Loaded.");
+        }
+        else
+        {
+            Debug.LogError("File not found: " + filePath);
+        }
+    }
+
+    public string GetDocumentContent()
+    {
+        return documentContent;
+    }
+}
